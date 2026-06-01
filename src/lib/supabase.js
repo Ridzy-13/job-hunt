6b26ef28dc36666abf39646880135b4cc85e87e4
Ridzy-13 <rshaw3@babson.edu>
2026-05-31 22:03:17 -0400

    jkjkjkjkjk

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -2,9 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   'https://ktzpfdwbvlaqtdjqgioa.supabase.co',
-  'sb_publishable_RPiuZWzr5LTtz3n3JOt81A_g67kmsrO',
-  {
-    realtime: { enabled: false },
-    global: { headers: {} },
-  }
+  'sb_publishable_RPiuZWzr5LTtz3n3JOt81A_g67kmsrO'
 )
